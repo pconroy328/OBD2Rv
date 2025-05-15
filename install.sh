7c1c919f4b2d0762727fa25210eb606ed692c657
Patrick Conroy <patrick@conroy-family.net>
2025-05-14 19:29:57 -0600

    dependecies

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+sudo apt install python3-pint
+sudo apt install python3-serial
+sudo ./startOBD2.sh
+
+
 sudo cp obd2.service /etc/systemd/system/.
 sudo chmod 644 /etc/systemd/system/obd2.service
 sudo systemctl daemon-reload
